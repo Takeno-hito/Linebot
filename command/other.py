98dc20f586fc90dd4f030b9c9d4a5716dd60559e
naaga0610 <ryugo.takemura@gmail.com>
2018-04-10 19:05:41 +0900

    辞書型の使い方を間違えていたので修正、

--- command/other.py
+++ command/other.py
@@ -1,26 +1,29 @@
+# -*- coding: utf-8 -*-
+
 import sys, os
 import json
 import requests
 sys.path.append(os.pardir)
 import botconfig
+import reply
 
 def bye(data):
     # data : type dic (line event)
-    if(data.source.type == 'user'):
+    if(data['source']['type'] == 'user'):
         pass
 
-    else if(data.source.type == 'group'):
-        replytext(data.replyToken, "ばいばい；；")
-        quitgroup(data.source.groupId)
+    elif(data['source']['type'] == 'group'):
+        reply.replytext(data['replyToken'], "ばいばい；；")
+        quitgroup(data['source']['groupId'])
 
-    else if(data.source.type == 'room'):
-        replytext(data.replyToken, "ばいばい；；")
-        quitroom(data.source.roomId)
+    elif(data['source']['type'] == 'room'):
+        reply.replytext(data['replyToken'], "ばいばい；；")
+        quitroom(data['source']['roomId'])
 
 def status(data):
-    if(data.source.type == 'user'):
+    if(data['source']['type'] == 'user'):
         header = "Bearer " + access_token
-        url = 'https://api.line.me/v2/bot/profile/' + data.source.userId
+        url = 'https://api.line.me/v2/bot/profile/' + data['source']['userId']
         response = requests.get(url, headers = header)
         resData = json.load(response)
 
@@ -29,14 +32,14 @@ def status(data):
         type : user
         userId : {}
         名前 : {}
-        '''.format(data.source.userId, resData.displayName)
-        replytext(data.replyToken, string)
+        '''.format(data['source']['userId'], resData['displayName'])
+        reply.replytext(data['replyToken'], string)
 
-    if(data.source.type == 'group'):
-        if('data.soure.userId' in locals()):
+    if(data['source']['type'] == 'group'):
+        if(userId in data['source']):
             header = "Bearer " + access_token
             url = 'https://api.line.me/v2/bot/group/{groupId}/member/{userId}'.format(
-                groupId = data.source.groupId, userId = data.source.userId)
+                groupId = data['source']['groupId'], userId = data['source']['userId'])
             response = requests.get(url, headers = header)
             resData = json.load(response)
 
@@ -48,8 +51,8 @@ def status(data):
             あなたの情報
             userId : {}
             名前 : {}
-            '''.format(data.source.groupId, data.source.userId, resData.displayName)
-            replytext(data.replyToken, string)
+            '''.format(data['source']['groupId'], data['source']['userId'], resData['displayName'])
+            reply.replytext(data['replyToken'], string)
 
         else:
             string = '''
@@ -63,7 +66,20 @@ def status(data):
 
             情報を得たい場合は、友達追加をして、LINEの公式アカウントの利用条件に同意する必要があります。
             同意する方法の一つに、このBotを追加する方法があります。
-            '''.format(data.source.groupId)
-            replytext(data.replyToken, string)
-    else if(data.source.type == 'room'):
+            '''.format(data['source']['groupId'])
+            reply.replytext(data['replyToken'], string)
+    elif(data['source']['type'] == 'room'):
         string = 'ルームでは、一部の機能が制限されています。グループに招待して使用するようお願いします。'
+
+def help(data):
+    pass
+
+def parrot(data):
+    #オウムがえし
+    pass
+
+def search(data):
+    pass
+
+def massageboard(data):
+    pass
