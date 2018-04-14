3ce40f6ce7937ca93bf49ca8142bab5bc79a2aad
naaga0610 <ryugo.takemura@gmail.com>
2018-04-15 08:32:29 +0900

    WebhookをWebhook機能だけにする

--- webhook.php
+++ webhook.php
@@ -7,23 +7,7 @@ $json_string = file_get_contents('php://input');
 
 $Path = 'python2.7 /home/kusanagi/WordPress/DocumentRoot/bot/line/dev/linebot/call.py \''.$json_string.'\'';
 
-exec($Path, $stdout, $return);
-
-echo $return.'<br>';
-
-echo $Path."<br>";
-
-echo $stdout.'<br>';
-
-var_dump($stdout);
-
-echo '<br>';
-
-
-echo count($stdout).'<br>';
-
-foreach($stdout as $str){
-	echo $stdout[$str];
-}
+exec($Path, $stdout, $retun);
+echo "It's Works!"
 
 ?>
