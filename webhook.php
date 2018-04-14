539d8cbd0b6f008cedcdd036291551f95749ac74
naaga0610 <ryugo.takemura@gmail.com>
2018-04-14 21:00:46 +0900

    webhookの修正

--- webhook.php
+++ webhook.php
@@ -5,8 +5,25 @@
 //コンテンツの取得
 $json_string = file_get_contents('php://input');
 
-$Path = 'python ./core/call.py ' + $json_string;
+$Path = 'python2.7 /home/kusanagi/WordPress/DocumentRoot/bot/line/dev/linebot/call.py \''.$json_string.'\'';
 
-exec($Path);
+exec($Path, $stdout, $return);
+
+echo $return.'<br>';
+
+echo $Path."<br>";
+
+echo $stdout.'<br>';
+
+var_dump($stdout);
+
+echo '<br>';
+
+
+echo count($stdout).'<br>';
+
+foreach($stdout as $str){
+	echo $stdout[$str];
+}
 
 ?>
