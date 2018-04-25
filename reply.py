a2e9587c95972c1fd8e6dc6011bfc27431e1f133
naaga0610 <ryugo.takemura@gmail.com>
2018-04-25 16:21:28 +0900

    logをつける

--- reply.py
+++ reply.py
@@ -8,7 +8,7 @@ import botconfig
 import json
 
 def replytext(Token, text):
-    #try:
+    try:
     print 'called:{}'.format(text)
     print Token
 
@@ -29,16 +29,16 @@ def replytext(Token, text):
     r = requests.post(endpoint, data=json.dumps(payload), headers=headers)
     print r.text
     print payload
-    #except:
-        # logger = logging.getLogger('LoggingTest')
-        # logger.setLevel(10)
-        # fh = logging.FileHandler('test.log')
-        # logger.addHandler(fh)
-        # sh = logging.StreamHandler()
-        # logger.addHandler(sh)
-        #
-        # formatter = logging.Formatter('%(asctime)s:%(lineno)d:%(levelname)s:%(message)s')
-        # fh.setFormatter(formatter)
-        # sh.setFormatter(formatter)
-        #
-        # logger.log(50, '')-)
+    except:
+        logger = logging.getLogger('LoggingTest')
+        logger.setLevel(10)
+        fh = logging.FileHandler('logger.log')
+        logger.addHandler(fh)
+        sh = logging.StreamHandler()
+        logger.addHandler(sh)
+
+        formatter = logging.Formatter('%(asctime)s:%(lineno)d:%(levelname)s:%(message)s')
+        fh.setFormatter(formatter)
+        sh.setFormatter(formatter)
+
+        logger.log(50, '')-)
