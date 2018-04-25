036e476ed5c318bdef9bc5c919fd7478e3bccda0
naaga0610 <ryugo.takemura@gmail.com>
2018-04-25 15:16:58 +0900

    pycファイルを作成しないように変更

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@
 //コンテンツの取得
 $json_string = file_get_contents('php://input');
 
-$Path = 'python2.7 2>&1 /home/kusanagi/WordPress/DocumentRoot/bot/line/dev/linebot/call.py \''.$json_string.'\'';
+$Path = 'python2.7 2>&1 -B /home/kusanagi/WordPress/DocumentRoot/bot/line/dev/linebot/call.py \''.$json_string.'\'';
 
 exec($Path, $stdout, $return);
 
