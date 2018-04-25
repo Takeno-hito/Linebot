ec88480aa18bfe4f372c25d3bc9b19999cf49e9b
naaga0610 <ryugo.takemura@gmail.com>
2018-04-25 18:00:31 +0900

    インデントミスしてたので修正

--- reply.py
+++ reply.py
@@ -9,26 +9,26 @@ import json
 
 def replytext(Token, text):
     try:
-    print 'called:{}'.format(text)
-    print Token
+        print 'called:{}'.format(text)
+        print Token
 
-    payload = {
-        "replyToken": "{}".format(Token),
-        "messages":[
-            {
-                "type":"text",
-                "text": "{}".format(text)
-            }
-        ]
-    }
-    endpoint = "https://api.line.me/v2/bot/message/reply"
-    headers = {
-        "Content-Type" : "application/json",
-        "Authorization" : "Bearer {}".format(botconfig.access_token)
-    }
-    r = requests.post(endpoint, data=json.dumps(payload), headers=headers)
-    print r.text
-    print payload
+        payload = {
+            "replyToken": "{}".format(Token),
+            "messages":[
+                {
+                    "type":"text",
+                    "text": "{}".format(text)
+                }
+            ]
+        }
+        endpoint = "https://api.line.me/v2/bot/message/reply"
+        headers = {
+            "Content-Type" : "application/json",
+            "Authorization" : "Bearer {}".format(botconfig.access_token)
+        }
+        r = requests.post(endpoint, data=json.dumps(payload), headers=headers)
+        print r.text
+        print payload
     except:
         logger = logging.getLogger('LoggingTest')
         logger.setLevel(10)
